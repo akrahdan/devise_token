92151509cf1e60adc2c65f993224d426c2297117
Samuel Akrah <akrahdan@gmail.com>
2017-12-03 14:56:53 +0000

    changes to authenticable

--- lib/devise_token.rb
+++ lib/devise_token.rb
@@ -2,6 +2,7 @@ require "devise"
 require "devise_token/engine"
 require "devise_token/controllers/helpers"
 require "devise_token/controllers/url_helpers"
+require "devise_token/controllers/authenticable"
 module DeviseToken
   # Your code goes here...
 end
