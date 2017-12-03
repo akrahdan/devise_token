0578de9762f07e1cd9298d5fe30aac162de455d0
Samuel Akrah <akrahdan@gmail.com>
2017-12-03 15:03:43 +0000

    changed authenticable load path

--- lib/devise_token.rb
+++ lib/devise_token.rb
@@ -2,7 +2,7 @@ require "devise"
 require "devise_token/engine"
 require "devise_token/controllers/helpers"
 require "devise_token/controllers/url_helpers"
-require "devise_token/controllers/authenticable"
+require "devise_token/authenticable"
 module DeviseToken
   # Your code goes here...
 end
