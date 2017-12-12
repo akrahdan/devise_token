45d0c1193c4cd720bc70d6cac941d8f64933ce14
Samuel Akrah <akrahdan@gmail.com>
2017-12-12 14:11:49 +0000

    edited authenticate_user

--- lib/devise_token/controllers/helpers.rb
+++ lib/devise_token/controllers/helpers.rb
@@ -38,7 +38,7 @@ module DeviseToken
         mapping = mapping.name
 
         class_eval <<-METHODS, __FILE__, __LINE__ + 1
-          def authenticate_#{mapping}!(opts={})
+          def authenticate_#{mapping}_token!(opts={})
             unless current_#{mapping}
               render_authenticate_error
             end
