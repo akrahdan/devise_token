637c1f6096ca097bc8d0b7abc0f268ada8a61bbc
Samuel Akrah <akrahdan@gmail.com>
2017-12-13 16:23:59 +0000

    modified authenticable

--- lib/devise_token/authenticable.rb
+++ lib/devise_token/authenticable.rb
@@ -3,11 +3,24 @@ module DeviseToken::Authenticable
     def authenticate_auth resource_class
       if token
         DeviseToken::JsonWebToken.new(token: token).current_resource(resource_class)
+      else
+         resource_class.find_by(access_token: access_token)
       end
     end
 
+
     def token
       params[:auth_token] || auth_token_request
+    end
+
+    def access_token
+      params[:access_token] || access_token_request
+    end
+
+    def access_token_request
+      unless request.headers['access_token'].nil?
+        request.headers.fetch("access_token")
+      end
     end
 
     def auth_token_request
@@ -15,6 +28,8 @@ module DeviseToken::Authenticable
         request.headers.fetch("Authorization", "").split(" ").last
       end
     end
+
+
 
     def auth_token_response
 
